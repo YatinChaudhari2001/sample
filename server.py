94916434823c39265403ce5572bf75bdaf663e78
yatin <yatinchaudhari13@gmail.com>
2023-07-28 21:20:47 +0530

    color change to blue

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def root():
-  return '<h1 style="color: red">#### Yohhh bro... this us my sample application for tommorows exam version 1</h1>'
+  return '<h1 style="color: blue">#### Yohhh bro... this us my sample application for tommorows exam version 1</h1>'
 
 
 app.run(port=2500, host="0.0.0.0", debug=True)
